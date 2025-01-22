58a9197c5f481fa9a6e02e98c86c880ff8e0d17e
John Ojal <jojal@KEKLF-LT2706-L.local>
2025-01-22 12:17:56 +0300

    starting time calculations

--- scripts/time_calculations_per cadre.R
+++ scripts/time_calculations_per cadre.R
@@ -3,9 +3,12 @@
 # Script to calculate times needs by each cadre
 #--------------------------------------------------------------
 
-
 # Load data
+load("outputs/data_hiv_time_standards.rds")
 
+# dummy dataset (for now) of hiv patients needing care in each county.
+patients_per_hiv_condition <- data.frame(condition=rep(unique(standards_data$disease_condition_risk_factor_public_health_function),47))
+patients_per_hiv_condition <- patients_per_hiv_condition |> group_by(condition) |> mutate(county=n)
 
 
 
